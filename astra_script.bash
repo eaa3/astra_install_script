c1dfa1c1e435d508a5554a7a076a3187d48b358f
Ermano Arruda <ermano.arruda@gmail.com>
2017-10-19 15:52:07 +0100

    version 1

---
+++ astra_script.bash
@@ -0,0 +1,14 @@
+mkdir -p astra_example_ws/src
+cd astra_example_ws/src
+git clone https://github.com/tonybaltovski/ros_astra_launch.git --branch upstream
+git clone https://github.com/tonybaltovski/ros_astra_camera.git --branch upstream
+cd ..
+rosdep install --from-paths src --ignore-src --rosdistro=indigo -y
+catkin_make
+source devel/setup.bash
+rosrun astra_camera create_udev_rules
+
+echo "###########################################"
+echo "Please reboot now"
+echo "###########################################"
+sudo reboot
