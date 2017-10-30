d2d91f85ff4c92020c24e39f7aba0d61a1f03ef0
Ermano Arruda <ermano.arruda@gmail.com>
2017-10-30 17:15:26 +0000

    Update astra_script.bash

--- astra_script.bash
+++ astra_script.bash
@@ -1,14 +1,14 @@
-#!/bin/bash
+!/bin/bash
 
-# mkdir -p astra_example_ws/src
-# cd astra_example_ws/src
-# git clone https://github.com/clearpathrobotics/ros_astra_launch.git --branch upstream
-# git clone https://github.com/clearpathrobotics/ros_astra_camera.git --branch upstream
-# cd ..
-# rosdep install --from-paths src --ignore-src --rosdistro=indigo -y
-# catkin_make
-# source devel/setup.bash
-# rosrun astra_camera create_udev_rules
+mkdir -p astra_example_ws/src
+cd astra_example_ws/src
+git clone https://github.com/clearpathrobotics/ros_astra_launch.git --branch upstream
+git clone https://github.com/clearpathrobotics/ros_astra_camera.git --branch upstream
+cd ..
+rosdep install --from-paths src --ignore-src --rosdistro=indigo -y
+catkin_make
+source devel/setup.bash
+rosrun astra_camera create_udev_rules
 
 echo "###########################################"
 echo "Please reboot. Do you want to reboot now (y/n)?"
@@ -19,7 +19,7 @@ test=`echo $answer | grep "^y"`
 if [ ! -z "$test" ] || [ "$test" == "y" ] 
 then
     echo "rebooting"
-    #sudo reboot
+    sudo reboot
 else
     echo "Remember to reboot later."
 fi
