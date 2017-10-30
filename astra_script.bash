0cef756e8d0c2fc10aa88ea464bd2e393250f198
Ermano Arruda <ermano.arruda@gmail.com>
2017-10-30 16:23:46 +0000

    ClearpathRobotics links

--- astra_script.bash
+++ astra_script.bash
@@ -1,7 +1,7 @@
 mkdir -p astra_example_ws/src
 cd astra_example_ws/src
-git clone https://github.com/tonybaltovski/ros_astra_launch.git --branch upstream
-git clone https://github.com/tonybaltovski/ros_astra_camera.git --branch upstream
+git clone https://github.com/clearpathrobotics/ros_astra_launch.git --branch upstream
+git clone https://github.com/clearpathrobotics/ros_astra_camera.git --branch upstream
 cd ..
 rosdep install --from-paths src --ignore-src --rosdistro=indigo -y
 catkin_make
