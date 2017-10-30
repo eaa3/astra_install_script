2866ff2d92cf682eee3088875fa2ac81b689a78b
Ermano Arruda <ermano.arruda@gmail.com>
2017-10-30 16:48:41 +0000

    fixing syntax error

--- astra_script.bash
+++ astra_script.bash
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-mkdir -p astra_example_ws/src
-cd astra_example_ws/src
-git clone https://github.com/clearpathrobotics/ros_astra_launch.git --branch upstream
-git clone https://github.com/clearpathrobotics/ros_astra_camera.git --branch upstream
-cd ..
-rosdep install --from-paths src --ignore-src --rosdistro=indigo -y
-catkin_make
-source devel/setup.bash
-rosrun astra_camera create_udev_rules
+# mkdir -p astra_example_ws/src
+# cd astra_example_ws/src
+# git clone https://github.com/clearpathrobotics/ros_astra_launch.git --branch upstream
+# git clone https://github.com/clearpathrobotics/ros_astra_camera.git --branch upstream
+# cd ..
+# rosdep install --from-paths src --ignore-src --rosdistro=indigo -y
+# catkin_make
+# source devel/setup.bash
+# rosrun astra_camera create_udev_rules
 
 echo "###########################################"
 echo "Please reboot. Do you want to reboot now (y/n)?"
 echo "###########################################"
 answer="n"
 read answer
-if echo "$answer" | grep -iq "^y" ;then
+if echo "$answer" | grep -iq "^y"; then
     echo "rebooting";
     #sudo reboot
 else
